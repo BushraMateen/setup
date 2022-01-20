5127d707e44ca208dea0fdc927b2cb8e0d5f8c21
mastanpatel <Bushra.mateen95@outlook.com>
2022-01-20 11:14:57 +0530

    button

--- src/Categories.js
+++ src/Categories.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Categories = ({filterItems}) => {
   return (
       <div className='btn-container'>
-       
+       <button className='filter-btn'>
                 </button>
           )
         })}
